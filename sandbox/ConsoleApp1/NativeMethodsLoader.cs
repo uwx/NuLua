c4a9064bebfb5f512969ac792ea8f62dde52c0ac
nuskey8 <nuskey.md@gmail.com>
2026-06-15 21:49:46 +0900

    fix: consoleapp1 error

---
+++ sandbox/ConsoleApp1/NativeMethodsLoader.cs
@@ -0,0 +1,72 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// for project reference loader
+internal static class NativeMethodsLoader
+{
+    // https://docs.microsoft.com/en-us/dotnet/standard/native-interop/cross-platform
+    // Library path will search
+    // win => __DllName, __DllName.dll
+    // linux, osx => __DllName.so, __DllName.dylib
+
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        NativeLibrary.SetDllImportResolver(
+            typeof(NuLua.Interop.Lua55.NativeMethods).Assembly,
+            DllImportResolver
+        );
+    }
+
+    static IntPtr DllImportResolver(
+        string libraryName,
+        Assembly assembly,
+        DllImportSearchPath? searchPath
+    )
+    {
+        if (libraryName is "lua55")
+        {
+            var name = libraryName;
+            string? ext;
+            string? prefix;
+            string? platform;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                platform = "win";
+                prefix = "";
+                ext = ".dll";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                platform = "osx";
+                prefix = libraryName.StartsWith("lib") ? "" : "lib";
+                ext = ".dylib";
+            }
+            else
+            {
+                platform = "linux";
+                prefix = libraryName.StartsWith("lib") ? "" : "lib";
+                ext = ".so";
+            }
+
+            var arch = RuntimeInformation.OSArchitecture switch
+            {
+                Architecture.Arm64 => "arm64",
+                Architecture.X64 => "x64",
+                Architecture.X86 => "x86",
+                _ => throw new NotSupportedException(),
+            };
+
+            return NativeLibrary.Load(
+                Path.Combine(
+                    AppContext.BaseDirectory,
+                    $"runtimes/{platform}-{arch}/native/{prefix}{name}{ext}"
+                )
+            );
+        }
+
+        return IntPtr.Zero;
+    }
+}
